55175c929816c705b667119f373d66f3b2c1b564
Tom <tom.hoad03@gmail.com>
2022-03-30 16:34:05 +0100

    Project and select

--- src/sjdb/Estimator.java
+++ src/sjdb/Estimator.java
@@ -1,15 +1,8 @@
 package sjdb;
 
 public class Estimator implements PlanVisitor {
-	public Estimator() {
-		// empty constructor
-	}
+	public Estimator() {}
 
-	/* 
-	 * Create output relation on Scan operator
-	 *
-	 * Example implementation of visit method for Scan operators.
-	 */
 	public void visit(Scan op) {
 		Relation input = op.getRelation();
 		Relation output = new Relation(input.getTupleCount());
@@ -22,14 +15,47 @@ public class Estimator implements PlanVisitor {
 	}
 
 	public void visit(Project op) {
+		Relation input = op.getInput().getOutput();
+		Relation output = new Relation(input.getTupleCount());
+
+		for (Attribute attribute : input.getAttributes()) {
+			if (op.getAttributes().contains(attribute)) {
+				output.addAttribute(new Attribute(attribute));
+			}
+		}
+
+		op.setOutput(output);
 	}
 	
 	public void visit(Select op) {
+		Relation input = op.getInput().getOutput();
+		Relation output;
+
+		if (op.getPredicate().equalsValue()) { // attr=val
+			Attribute predicateAttribute = input.getAttribute(op.getPredicate().getLeftAttribute());
+			int predicateAttributeCount = predicateAttribute.getValueCount();
+			output = new Relation(input.getTupleCount() - predicateAttributeCount);
+
+			for (Attribute attribute : input.getAttributes()) {
+				if (attribute.equals(predicateAttribute)) {
+					output.addAttribute(new Attribute(attribute.getName(), 1)); // set number of distinct values to 1
+				} else {
+					output.addAttribute(new Attribute(attribute));
+				}
+			}
+		} else { // attr=attr
+			Attribute leftPredicateAttribute = input.getAttribute(op.getPredicate().getLeftAttribute());
+			Attribute rightPredicateAttribute = input.getAttribute(op.getPredicate().getRightAttribute());
+		}
+
+		op.setOutput(output);
 	}
 	
 	public void visit(Product op) {
+
 	}
 	
 	public void visit(Join op) {
+
 	}
 }
