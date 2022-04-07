f657bd7991bec3d6dc7ee397786c622fba507c1b
Tom <tom.hoad03@gmail.com>
2022-04-07 15:24:30 +0100

    Move select operators down

--- src/sjdb/Optimiser.java
+++ src/sjdb/Optimiser.java
@@ -13,62 +13,116 @@ public class Optimiser {
 
     1. Start with canonical form.
     2. Move select operators down.
+        a. Pushed down to just above the relation that contains attr when attr=val.
+        b. Pushed down to the product above the subtree containing the relations that contain attr1 and attr2 when attr=attr.
     3. Reorder subtrees to put most restrictive select first.
     4. Combine product and select to create join.
     5. Move project operators down.
      */
 
     public Operator optimise(Operator plan) {
+        Operator newPlan = plan;
+
         if (plan.toString().startsWith("SELECT")) {
             Select selectPlan = (Select) plan;
             Predicate selectPredicate = selectPlan.getPredicate();
 
-            // pushed down to just above the relation that contains attr
+            // Move select operators down
             if (selectPredicate.equalsValue()) {
-                System.out.println("TEST1");
-            // pushed down to the product above the subtree containing the relations that contain attr1 and attr2
+                newPlan = moveSelectDown1(selectPlan.getInput(), selectPredicate);
             } else {
-                System.out.println("TEST2");
-                moveSelectDown(selectPlan.getInput(), selectPredicate.getLeftAttribute(), selectPredicate.getRightAttribute());
+                newPlan = moveSelectDown2(selectPlan.getInput(), selectPredicate);
             }
-
-            optimise(selectPlan.getInput());
-        } else {
-            System.out.println("ELSE");
+            selectResolved = false;
         }
 
-        return plan;
+        try {
+            return newPlan;
+        } catch (Exception e1) {
+            try {
+                Join joinPlan = (Join) newPlan;
+                return new Join(optimise(joinPlan.getLeft()), optimise(joinPlan.getRight()), joinPlan.getPredicate());
+            } catch (Exception e2) {
+                try {
+                    Select selectPlan = (Select) plan;
+                    return new Select(optimise(selectPlan.getInput()), selectPlan.getPredicate());
+                } catch (Exception e3) {
+                    try {
+                        Project projectPlan = (Project) plan;
+                        return new Project(optimise(projectPlan.getInput()), projectPlan.getAttributes());
+                    } catch (Exception e4) {
+                        Product productPlan = (Product) plan;
+                        return new Product(optimise(productPlan.getLeft()), optimise(productPlan.getRight()));
+                    }
+                }
+            }
+        }
     }
 
-    public Operator moveSelectDown(Operator plan, Attribute leftAttribute, Attribute rightAttribute) {
+    public Operator moveSelectDown1(Operator plan, Predicate predicate) {
+        if (!selectResolved) {
+            try {
+                Scan scanOp = (Scan) plan;
+
+                if (scanOp.getOutput().getAttributes().contains(predicate.getLeftAttribute())) {
+                    selectResolved = true;
+                    return new Select(scanOp, predicate);
+                } else {
+                    return plan;
+                }
+            } catch (Exception e1) {
+                try {
+                    Join joinOp = (Join) plan;
+                    return new Join(moveSelectDown1(joinOp.getLeft(), predicate), moveSelectDown1(joinOp.getRight(), predicate), joinOp.getPredicate());
+                } catch (Exception e2) {
+                    try {
+                        Select selectOp = (Select) plan;
+                        return new Select(moveSelectDown1(selectOp.getInput(), predicate), selectOp.getPredicate());
+                    } catch (Exception e3) {
+                        try {
+                            Project projectOp = (Project) plan;
+                            return new Project(moveSelectDown1(projectOp.getInput(), predicate), projectOp.getAttributes());
+                        } catch (Exception e4) {
+                            Product productOp = (Product) plan;
+                            return new Product(moveSelectDown1(productOp.getLeft(), predicate), moveSelectDown1(productOp.getRight(), predicate));
+                        }
+                    }
+                }
+            }
+        } else {
+            return plan;
+        }
+    }
+
+    public Operator moveSelectDown2(Operator plan, Predicate predicate) {
         if (!selectResolved) {
             try {
                 Product productOp = (Product) plan;
 
                 // Check if left and right subtrees contain attributes, otherwise find next product
-                Boolean leftLeft = leftAttrLeft(productOp.getLeft(), leftAttribute);
-                Boolean rightRight = rightAttrRight(productOp.getRight(), rightAttribute);
+                Boolean leftLeft = isInSubtree(productOp.getLeft(), predicate.getLeftAttribute());
+                Boolean rightRight = isInSubtree(productOp.getRight(), predicate.getRightAttribute());
 
-                if (leftLeft && rightRight) {
+                if ((leftLeft && rightRight) || (!leftLeft && !rightRight)) {
                     selectResolved = true;
-                    return plan;
+                    return new Select(productOp, predicate);
                 } else if (leftLeft) {
-                    return new Product(moveSelectDown(productOp.getLeft(), leftAttribute, rightAttribute), productOp.getRight());
+                    return new Product(moveSelectDown2(productOp.getLeft(), predicate), productOp.getRight());
                 } else {
-                    return new Product(productOp.getLeft(), moveSelectDown(productOp.getRight(), leftAttribute, rightAttribute));
+                    return new Product(productOp.getLeft(), moveSelectDown2(productOp.getRight(), predicate));
                 }
             } catch (Exception e1) {
                 try {
                     Join joinOp = (Join) plan;
-                    return new Join(moveSelectDown(joinOp.getLeft(), leftAttribute, rightAttribute), moveSelectDown(joinOp.getRight(), leftAttribute, rightAttribute), joinOp.getPredicate());
+                    return new Join(moveSelectDown2(joinOp.getLeft(), predicate), moveSelectDown2(joinOp.getRight(), predicate), joinOp.getPredicate());
                 } catch (Exception e2) {
                     try {
                         Select selectOp = (Select) plan;
-                        return new Select(moveSelectDown(selectOp.getInput(), leftAttribute, rightAttribute), selectOp.getPredicate());
+                        return new Select(moveSelectDown2(selectOp.getInput(), predicate), selectOp.getPredicate());
                     } catch (Exception e3) {
                         try {
                             Project projectOp = (Project) plan;
-                            return new Project(moveSelectDown(projectOp.getInput(), leftAttribute, rightAttribute), projectOp.getAttributes());
+                            return new Project(moveSelectDown2(projectOp.getInput(), predicate), projectOp.getAttributes());
                         } catch (Exception e4) {
                             return plan;
                         }
@@ -80,28 +134,15 @@ public class Optimiser {
         }
     }
 
-    public Boolean leftAttrLeft(Operator leftPlan, Attribute leftAttribute) {
+    public Boolean isInSubtree(Operator plan, Attribute attribute) {
         try {
-            Scan scanOp = (Scan) leftPlan;
-            return scanOp.getOutput().getAttributes().contains(leftAttribute);
+            Scan scanOp = (Scan) plan;
+            return scanOp.getOutput().getAttributes().contains(attribute);
         } catch (Exception e) {
-            if (leftPlan.getInputs().size() > 1) {
-                return leftAttrLeft(leftPlan.getInputs().get(0), leftAttribute) || leftAttrLeft(leftPlan.getInputs().get(1), leftAttribute);
+            if (plan.getInputs().size() > 1) {
+                return isInSubtree(plan.getInputs().get(0), attribute) || isInSubtree(plan.getInputs().get(1), attribute);
             } else {
-                return leftAttrLeft(leftPlan.getInputs().get(0), leftAttribute);
-            }
-        }
-    }
-
-    public Boolean rightAttrRight(Operator rightPlan, Attribute rightAttribute) {
-        try {
-            Scan scanOp = (Scan) rightPlan;
-            return scanOp.getOutput().getAttributes().contains(rightAttribute);
-        } catch (Exception e) {
-            if (rightPlan.getInputs().size() > 1) {
-                return rightAttrRight(rightPlan.getInputs().get(0), rightAttribute) || leftAttrLeft(rightPlan.getInputs().get(1), rightAttribute);
-            } else {
-                return rightAttrRight(rightPlan.getInputs().get(0), rightAttribute);
+                return isInSubtree(plan.getInputs().get(0), attribute);
             }
         }
     }
--- src/sjdb/Test.java
+++ src/sjdb/Test.java
@@ -27,32 +27,41 @@ public class Test {
 	
 	public static Catalogue createCatalogue() {
 		Catalogue cat = new Catalogue();
-		cat.createRelation("A", 100);
-		cat.createAttribute("A", "a1", 100);
-		cat.createAttribute("A", "a2", 15);
-		cat.createRelation("B", 150);
-		cat.createAttribute("B", "b1", 150);
-		cat.createAttribute("B", "b2", 100);
-		cat.createAttribute("B", "b3", 5);
+
+		cat.createRelation("Employee", 3);
+		cat.createAttribute("Employee", "ESSN", 100);
+		cat.createAttribute("Employee", "BDATE", 100);
+		cat.createAttribute("Employee", "LNAME", 15);
+
+		cat.createRelation("Works_On", 3);
+		cat.createAttribute("Works_On", "SSN", 100);
+		cat.createAttribute("Works_On", "PNO", 15);
+
+		cat.createRelation("Project", 3);
+		cat.createAttribute("Project", "PNUMBER", 100);
+		cat.createAttribute("Project", "PNAME", 15);
 		
 		return cat;
 	}
 
 	public static Operator query(Catalogue cat) throws Exception {
-		Scan a = new Scan(cat.getRelation("A"));
-		Scan b = new Scan(cat.getRelation("B")); 
-		
+		Scan a = new Scan(cat.getRelation("Employee"));
+		Scan b = new Scan(cat.getRelation("Works_On"));
+		Scan c = new Scan(cat.getRelation("Project"));
+
 		Product p1 = new Product(a, b);
+		Product p2 = new Product(p1, c);
 		
-		Select s1 = new Select(p1, new Predicate(new Attribute("a2"), new Attribute("b3")));
-		
-		ArrayList<Attribute> atts = new ArrayList<Attribute>();
-		atts.add(new Attribute("a2"));	
-		atts.add(new Attribute("b1"));	
-		
-		Project plan = new Project(s1, atts);
-		
-		return s1;
+		Select s1 = new Select(p2, new Predicate(new Attribute("PNUMBER"), new Attribute("PNO")));
+		Select s2 = new Select(s1, new Predicate(new Attribute("PNAME"), "Aquarius"));
+
+		// ArrayList<Attribute> atts = new ArrayList<Attribute>();
+		// atts.add(new Attribute("a2"));
+		// atts.add(new Attribute("b1"));
+		//
+		// Project plan = new Project(s1, atts);
+
+		return s2;
 	}
 	
 }
